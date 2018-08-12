e5831bc054f3c76780aba70337e7a785f1c9b3b0
makbari.it <makbari.it@outlook.com>
2018-08-12 11:21:06 +0430

    trivial change

--- Share/Services.cs
+++ Share/Services.cs
@@ -10,6 +10,7 @@ namespace Share
             {
                 var factory = new ConnectionFactory()
                 {
+                    //Uri = new Uri(@"amqps://rumacvwx:ELu2aFDH_h0lrx1UoW1rrUg7BrqkR6t1@baboon.rmq.cloudamqp.com/rumacvwx"),
                     HostName = "localhost",
                     Port = 5672,
                     UserName = "guest",
